f6c2b79bf411d52a836f975f8688d98b932aec70
ratnesh <ratnesh.kumbhkar@gmail.com>
2020-04-18 10:09:50 -0700

    Fixed first quad
    - Increased the image size
    - removed the multiplication factor of x and y axis
    - using only linear combination of the nodes of the next edge

--- test1.py
+++ test1.py
@@ -3,7 +3,7 @@ import math
 import cairo
 import random
 
-WIDTH, HEIGHT = 512, 512
+WIDTH, HEIGHT = 1024, 1024
 
 surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, WIDTH, HEIGHT)
 ctx = cairo.Context(surface)
@@ -18,8 +18,6 @@ max_x = 0.7
 delta = 0.05
 edge_x = [0]*5 #assuming 4 edges
 edge_y = [0]*5
-xfctr = [1, -1, -1, 1]
-yfctr = [1, 1, -1, -1]
 
 random.seed(1)
 # Get 4 random coordinates for 4 edges
@@ -46,13 +44,20 @@ edge_y[2]=by
 edge_x[3]=0
 edge_y[3]=a2
 ctx.move_to(edge_x[0], edge_y[0])
-for loop in range(0,50):
+for loop in range(0,100):
 	idx = (loop)%4
-	nxt_x = edge_x[(idx+1)%4]*(1-delta) + xfctr[(idx+qd)%4]*edge_x[(idx+2)%4]*delta
-	nxt_y = edge_y[(idx+1)%4]*(1-delta) + yfctr[(idx+qd)%4]*edge_y[(idx+2)%4]*delta
+	#print("------------------")
+	#print(edge_x[idx], " ", edge_y[idx])
+	#print(edge_x[(idx+1)%4], " ", edge_y[(idx+1)%4])
+	#print(edge_x[(idx+2)%4], " ", edge_y[(idx+2)%4])
+	nxt_x = edge_x[(idx+1)%4]*(1-delta) + edge_x[(idx+2)%4]*delta
+	nxt_y = edge_y[(idx+1)%4]*(1-delta) + edge_y[(idx+2)%4]*delta
+	#print(nxt_x, " ", nxt_y)
 	edge_x[(idx+1)%4] = nxt_x
 	edge_y[(idx+1)%4] = nxt_y
 	ctx.line_to(nxt_x, nxt_y)
+	delta = min(1.05*delta, 0.5)
+	#print (delta)
 	#ctx.close_path()
 
 ctx.move_to(0, a2)
@@ -64,7 +69,7 @@ ctx.line_to(bx, by)  # Line to (x,y)
 
 
 #ctx.set_source_rgb(0.3, 0.2, 0.5)  # Solid color
-ctx.set_line_width(0.005)
+ctx.set_line_width(0.002)
 ctx.stroke()
 
 surface.write_to_png("example.png")  # Output to PNG
